edd554816a21d94bc2a575444e95ee6a8babd36d
Tintineglb <99283087+Tintineglb@users.noreply.github.com>
2026-01-08 15:49:49 +0100

    Add files via upload

---
+++ script_maps.js
@@ -0,0 +1,57 @@
+function updateImagePosition() {
+  const scrollY = window.scrollY;
+
+  const limite = 600; // fin de l’animation
+
+  // Position de départ
+  const xStart = 895;
+  const yStart = 480;
+  const scaleStart = 5;
+
+  // Position d’arrivée
+  const xEnd = 75;
+  const yEnd = 25;
+  const scaleEnd = 2.5;
+
+  // Calcul du pourcentage d’avancement (entre 0 et 1)
+  let t = scrollY / limite;
+  t = Math.max(0, Math.min(1, t)); // clamp
+  t = smoothstep(t);
+
+  // Interpolation linéaire
+  const x = xStart + (xEnd - xStart) * t;
+  const y = yStart + (yEnd - yStart) * t;
+  const scale = scaleStart + (scaleEnd - scaleStart) * t;
+
+  // Application du mouvement
+  document.getElementById("logo_navbar_maps").style.transform =
+    `translate(${x}px, ${y}px) scale(${scale})`;
+};
+
+function updateOpacity() {
+  const scrollY = window.scrollY;
+
+  const limite = 600; // fin de l’animation
+
+  const opacityStart = 0.5;
+  const opacityEnd = 0;
+
+  // Calcul du pourcentage d’avancement (entre 0 et 1)
+  let t = scrollY / limite;
+  t = Math.max(0, Math.min(1, t)); // clamp
+
+  // Interpolation linéaire
+  const opacity = opacityStart + (opacityEnd - opacityStart) * t;
+
+  document.getElementById("bg_fade").style.opacity = opacity;
+}
+
+window.addEventListener("scroll", updateOpacity);
+window.addEventListener("load", updateOpacity);
+
+window.addEventListener("scroll", updateImagePosition);
+window.addEventListener("load", updateImagePosition);
+
+function smoothstep(t) {
+  return t * t * (3 - 2 * t);
+}
